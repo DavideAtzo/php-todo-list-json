f721da908bd97dd2b1f1d2b7e76a27f53cc5da94
DavideAtzo <atzo9477@gmail.com>
2023-04-12 18:15:45 +0200

    stampa lista todo con ciclo

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            todo: [],
         }
+    },
+    created() {
+        axios.get('server.php')
+        .then((response) => {
+            this.todo = response.data;
+            console.log(response.data)
+        })
     },
     
 }).mount('#app')
--- server.php
+++ server.php
@@ -2,6 +2,7 @@
 $data = file_get_contents(__DIR__.'/data.json');
 
 $data = json_decode($data, true);
-var_dump($data)
-
-?>
+// var_dump($data)
+$data= json_encode($data);
+header('Content-Type: application/json');
+    echo $data;
